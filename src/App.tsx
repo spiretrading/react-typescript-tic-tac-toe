17cb4aba009523d19b17e816edf68147d257a1fa
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2019-05-28 14:06:12 -0400

    Solved the winner identification.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
 
-enum Display {
-  MOBILE,
-  LAPTOP,
-  DESKTOP
-}
-
 enum Mode {
   PLAY,
   GAME_OVER
@@ -30,19 +24,6 @@ const boardMap = [
   [[7, 6], [5, 2], [4, 0]]
 ];
 
-/** Indicates a player. */
-class Player {
-
-  /** Constructs a specific player.
-   * @param name - The name of the player.
-   */
-  public constructor(name: string) {
-    this._name = name;
-  }
-
-  private _name: string;
-}
-
 interface CellProps {
   display?: string
   piece: Piece
@@ -55,15 +36,11 @@ class Cell extends React.Component<CellProps> {
     onClick: () => {}
   }
 
-  constructor(props: CellProps){
-    super(props);
-  }
-
   public render(): JSX.Element {
     const content = (() => {
-      if(this.props.piece == Piece.NONE) {
+      if(this.props.piece === Piece.NONE) {
         return ''
-      } else if(this.props.piece == Piece.X) {
+      } else if(this.props.piece === Piece.X) {
         return 'X'
       } else {
         return 'O'
@@ -108,17 +85,23 @@ class Board extends React.Component<{}, BoardState> {
       mode: Mode.PLAY
     };
     this.onClick = this.onClick.bind(this);
+    this.isOver = this.isOver.bind(this);
+    this.reStart = this.reStart.bind(this);
   }
 
-  private isOver(index: number) {
-    const piece = this.state.lastPiece;
+  private isOver(index: number, lastPiece: Piece) {
+    const piece = lastPiece;
     var pathList = boardMap[index];
     var result: boolean;
+    console.log(piece);
     for(let i = 0; i < pathList.length; i++) {
       result = true;
+      console.log(pathList[i]);
       for(let j = 0; j < pathList[i].length; j++) {
-        result = pathList[i][j] === piece && result;
+        console.log(this.state.cellContents[pathList[i][j]], piece, result);
+        result = (this.state.cellContents[pathList[i][j]] === piece) && result;
       }
+      console.log(result);
       if(result) {
         return true;
       }
@@ -127,57 +110,79 @@ class Board extends React.Component<{}, BoardState> {
   }
 
   private onClick(index: number) {
-    console.log(index);
     if(this.state.cellContents[index] === Piece.NONE) {
-      this.state.cellContents[index] = (() => {
+      const newCell = (() => {
         if(this.state.lastPiece === Piece.NONE) {
           return Piece.X
-        } else if(this.state.lastPiece == Piece.X) {
+        } else if(this.state.lastPiece === Piece.X) {
           return Piece.O
         } else {
           return Piece.X
         }
       })();
+      var newContents: Piece[] = [];
+      for(var i = 0; i < this.state.cellContents.length; i++) {
+        if(i === index) {
+          newContents[i] = newCell;
+        } else {
+          newContents[i] = this.state.cellContents[i];
+        }
+      }
       this.setState({
-        lastPiece: this.state.cellContents[index],
-        cellContents: this.state.cellContents
+        lastPiece: newCell,
+        cellContents: newContents
       });
-      if(this.isOver(index)) {
-
+      console.log(index, newCell, this.isOver(index, newCell));
+      const result = this.isOver(index, newCell);
+      if(result) {
+        this.setState({ mode: Mode.GAME_OVER });
       }
     }
+  }
+
+  private reStart() {
+    this.setState({
+      lastPiece: Piece.NONE,
+      cellContents: [Piece.NONE, Piece.NONE, Piece.NONE, Piece.NONE, Piece.NONE,
+        Piece.NONE, Piece.NONE, Piece.NONE, Piece.NONE],
+      mode: Mode.PLAY
+    });
   }
 
   public render(): JSX.Element {
     if(this.state.mode === Mode.PLAY) {
       return (
-        <div style={Board.FLEX_ROW_OUTER_CONTAINER_STYLE}>
-          <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
-            <Cell piece={this.state.cellContents[0]} onClick={
-              () => this.onClick(0)}/>
-            <Cell piece={this.state.cellContents[1]} onClick={
-              () => this.onClick(1)}/>
-            <Cell piece={this.state.cellContents[2]} onClick={
-              () => this.onClick(2)}/>
+        <div>
+          <div style={Board.FLEX_ROW_OUTER_CONTAINER_STYLE}>
+            <h1>Two Player Tic Tac Toe</h1>
           </div>
-          <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
-            <Cell piece={this.state.cellContents[3]} onClick={
-              () => this.onClick(3)}/>
-            <Cell piece={this.state.cellContents[4]} onClick={
-              () => this.onClick(4)}/>
-            <Cell piece={this.state.cellContents[5]} onClick={
-              () => this.onClick(5)}/>
+          <div style={Board.FLEX_ROW_OUTER_CONTAINER_STYLE}>
+            <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
+              <Cell piece={this.state.cellContents[0]} onClick={
+                () => this.onClick(0)}/>
+              <Cell piece={this.state.cellContents[1]} onClick={
+                () => this.onClick(1)}/>
+              <Cell piece={this.state.cellContents[2]} onClick={
+                () => this.onClick(2)}/>
+            </div>
+            <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
+              <Cell piece={this.state.cellContents[3]} onClick={
+                () => this.onClick(3)}/>
+              <Cell piece={this.state.cellContents[4]} onClick={
+                () => this.onClick(4)}/>
+              <Cell piece={this.state.cellContents[5]} onClick={
+                () => this.onClick(5)}/>
+            </div>
+            <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
+              <Cell piece={this.state.cellContents[6]} onClick={
+                () => this.onClick(6)}/>
+              <Cell piece={this.state.cellContents[7]} onClick={
+                () => this.onClick(7)}/>
+              <Cell piece={this.state.cellContents[8]} onClick={
+                () => this.onClick(8)}/>
+            </div>
           </div>
-          <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
-            <Cell piece={this.state.cellContents[6]} onClick={
-              () => this.onClick(6)}/>
-            <Cell piece={this.state.cellContents[7]} onClick={
-              () => this.onClick(7)}/>
-            <Cell piece={this.state.cellContents[8]} onClick={
-              () => this.onClick(8)}/>
-          </div>
-        </div>
-      );
+        </div>);
     } else {
       const text = (() => {
         if(this.state.lastPiece === Piece.O) {
@@ -187,7 +192,8 @@ class Board extends React.Component<{}, BoardState> {
         }
       })();
       return (
-        <div style={Board.FLEX_COLUMN_CONTAINER_STYLE}>
+        <div style={Board.FLEX_COLUMN_OVER_STYLE} onClick={
+            () => this.reStart()}>
           <div style={Board.FLEX_ROW_STYLE}>
             <p style={Board.FONT_STYLE}>{text}</p>
           </div>
@@ -218,6 +224,17 @@ class Board extends React.Component<{}, BoardState> {
     height: '100%'
   }
 
+  private static readonly FLEX_COLUMN_OVER_STYLE = {
+    boxSizing: 'border-box' as 'border-box',
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    justifyContent: 'center' as 'center',
+    alignItems: 'center' as 'center',
+    height: '100%',
+    width: '100%',
+    marginTop: '90px'
+  }
+
   private static readonly FLEX_ROW_STYLE = {
     boxSizing: 'border-box' as 'border-box',
     display: 'flex',
@@ -225,10 +242,12 @@ class Board extends React.Component<{}, BoardState> {
     justifyContent: 'center' as 'center',
     alignItems: 'center' as 'center',
     width: '100%',
-    height: '100%'
+    //height: '100%'
   }
 
   private static readonly FONT_STYLE = {
+    width: '450px',
+    height: '150px',
     backgroundColor: 'blue',
     margin: '0',
     textAlign: 'center' as 'center',
@@ -242,7 +261,6 @@ class Board extends React.Component<{}, BoardState> {
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Two Player Tic Tac Toe</h1>
       <Board/>
     </div>
   );
